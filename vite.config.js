74693d789ac7a54f8c75edc2d0dab44155fc51c7
berserker1984 <laerte.mancio@gmail.com>
2025-09-13 06:25:52 -0300

    deploy vercel

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         //target: "http://localhost:3001", // backend local
-        target: "https://gerador-presell.vercel.app",
+        target: "https://gerador-presell.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
